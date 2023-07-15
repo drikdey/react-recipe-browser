11e2d7cc99a557beb351fd2bc6e8bd3766deed1a
Subham Mukhopadhyay <86921732+mukherjee-subham@users.noreply.github.com>
2023-07-16 00:49:21 +0530

    Styled buttons component, added hover and on click color

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -7,22 +7,22 @@ import { NavLink } from "react-router-dom";
 function Buttons() {
   return (
     <List>
-      <NavLink to={"/cuisine/Italian"}>
+      <SLink to={"/cuisine/Italian"}>
         <FaPizzaSlice />
-        <h3>Italian</h3>
-      </NavLink>
-      <NavLink to={"/cuisine/Mexican"}>
+        <h4>Italian</h4>
+      </SLink>
+      <SLink to={"/cuisine/Mexican"}>
         <FaPepperHot />
-        <h3>Mexican</h3>
-      </NavLink>
-      <NavLink to={"/cuisine/American"}>
+        <h4>Mexican</h4>
+      </SLink>
+      <SLink to={"/cuisine/American"}>
         <GiHamburger />
-        <h3>American</h3>
-      </NavLink>
-      <NavLink to={"/cuisine/Chinese"}>
+        <h4>American</h4>
+      </SLink>
+      <SLink to={"/cuisine/Chinese"}>
         <GiChopsticks />
-        <h3>Chinese</h3>
-      </NavLink>
+        <h4>Chinese</h4>
+      </SLink>
     </List>
   );
 }
@@ -31,6 +31,52 @@ const List = styled.div`
   display: flex;
   margin: 2rem 0rem;
   justify-content: center;
+`;
+
+const SLink = styled(NavLink)`
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-decoration: none;
+  border-radius: 50%;
+  align-items: center;
+  cursor: pointer;
+  width: 6rem;
+  height: 6rem;
+  transform: scale(0.8);
+  background: linear-gradient(45deg, #494949, #313131);
+
+  svg {
+    color: white;
+    font-size: 1.5rem;
+    z-index: 10;
+    position: absolute;
+    bottom: 40%;
+  }
+
+  h4 {
+    color: white;
+    font-size: 0.9rem;
+    position: absolute;
+    bottom: 18%;
+  }
+
+  &.active {
+    background: red;
+
+    svg {
+      color: whitesmoke;
+    }
+    h4 {
+      color: whitesmoke;
+    }
+  }
+
+  &:hover {
+    background: linear-gradient(45deg, #fd1d1d, #ff8400);
+  }
 `;
 
 export default Buttons;
