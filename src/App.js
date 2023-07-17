1eda3841a9dd96e14fe28e5bb34962816abb41b4
drikdey <drikdey292@gmail.com>
2023-07-18 00:44:10 +0530

    functionality

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import Pages from "./pages/Pages";
 import Buttons from "./components/Buttons";
 import styled from "styled-components";
 import Search from "./components/Search";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, NavLink } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <div style={{display: "flex", justifyContent: "space-around"}}>
+        <NavLink to={"/"}>
         <Logo
           className="appLogo"
           src={require("./assets/appLogo.png")}
           alt="Logo"
         />
+        </NavLink>
         <Search />
         </div>
         <Buttons />
